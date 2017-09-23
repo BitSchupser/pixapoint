d2a2133bff72a2683312cf902698fe7aa48a1b73
Alexander Rampp <dev@axgr.de>
2017-09-23 10:10:14 +0200

    Integrate SearchResultCollection into image-query-service

--- src/app/service/image-query.service.ts
+++ src/app/service/image-query.service.ts
@@ -1,3 +1,4 @@
+import { SearchResultCollection } from './search-result-collection';
 import { Base64coder } from './base64coder';
 import { Config } from './../config';
 import { Observable, Subject } from 'rxjs/Rx';
@@ -13,9 +14,10 @@ export class ImageQueryService {
   constructor(private config: Config, private http: HttpClient, private base64: Base64coder) { }
 
   search(queryString: string): Observable<SearchResult[]> {
-
+    const page = 1;
+    const resultsPerPage = 20;
     return this.http.get(this.baseURL + '&q="' + queryString + '"&response_group=high_resolution', {responseType: 'json'})
-      .map(result => this.createSearchResults(result));
+      .map(result => this.createSearchResults(result, page, resultsPerPage).results);
   }
 
   getAsBase64(toDownload: SearchResult): Observable<string> {
@@ -27,7 +29,9 @@ export class ImageQueryService {
     });
   }
 
-  private createSearchResults(apiResponse: any): any {
-    return apiResponse.hits.map(r => new SearchResult(r.previewURL, r.webformatURL, r.webformatWidth, r.webformatHeight));
+  private createSearchResults(apiResponse: any, page: number, resultsPerPage: number): SearchResultCollection {
+    const searchResults = apiResponse.hits.map(r => new SearchResult(r.previewURL, r.webformatURL, r.webformatWidth, r.webformatHeight));
+    const totalResults = apiResponse.total;
+    return new SearchResultCollection(searchResults, totalResults, page, resultsPerPage);
   }
 }

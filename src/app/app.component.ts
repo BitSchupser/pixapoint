4437740cfbade40aba38b84d4350a7cb947b65b0
Alexander Rampp <dev@axgr.de>
2017-10-06 15:20:55 +0200

    Add 'Searching...' label while waiting for search results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { CATCH_ERROR_VAR } from '@angular/compiler/src/output/output_ast';
 import { SearchResultCollection } from './service/search-result-collection';
 import * as querystring from 'querystring';
 import { SearchResult } from './service/search-result';
 import { ImageQueryService } from './service/image-query.service';
 import { Component, PACKAGE_ROOT_URL } from '@angular/core';
 import 'rxjs/add/operator/map';
+
+declare var fabric: any;
 
 @Component({
   selector: 'app-root',
@@ -18,13 +21,17 @@ export class AppComponent {
   get hasNextPage(): boolean { return this.result.hasNextPage(); }
   get searchResults(): SearchResult[] { return this.result.results; }
   get totalResults(): number { return this.result.totalResults; }
+  isSearching = false;
 
   constructor(private imageQuery: ImageQueryService) {
   }
 
   search(queryString: string, page: number = 1): void {
+    this.isSearching = true;
+
     this.imageQuery.search(queryString, page)
       .subscribe(sr => {
+        this.isSearching = false;
         this.result = sr;
       },
       err => console.error(err));
